bc3c1e4c05f20c7103c6c865896d7bc92cf9bf0d
Thilan Randika <dgtrandika@gmail.com>
2025-04-25 23:28:30 +0530

    Delivery driver login page

--- delivery-rider-frontend/src/App.tsx
+++ delivery-rider-frontend/src/App.tsx
@@ -1,17 +1,12 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-
   return (
-    <>
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm text-center mx-auto mt-12">
-      <h2 className="text-2xl font-bold text-gray-800">Delivery Rider Dashboard</h2>
-      <p className="text-gray-600 mt-3">
-        This is the interface for delivery riders.
-      </p>
-    </div>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
