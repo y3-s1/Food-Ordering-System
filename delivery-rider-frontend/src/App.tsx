959fe7b86a66b33fffbd4ca61828608198bbfbc9
Thilan Randika <dgtrandika@gmail.com>
2025-04-25 22:37:44 +0530

    Initiate delivery-rider-frontend (VITE + Typescript + Tailwind)

---
+++ delivery-rider-frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm text-center mx-auto mt-12">
+      <h2 className="text-2xl font-bold text-gray-800">Delivery Rider Dashboard</h2>
+      <p className="text-gray-600 mt-3">
+        This is the interface for delivery riders.
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
