7f737f09d65132941d46b8fd379e0f319eeb4587
Thilan Randika <dgtrandika@gmail.com>
2025-04-30 23:30:27 +0530

    fix login api

--- delivery-rider-frontend/src/api/auth.ts
+++ delivery-rider-frontend/src/api/auth.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export const loginRider = async (email: string, password: string) => {
   const res = await axios.post(
-    'http://localhost:8081/api/users/api/auth/login',
-    { withCredentials: true },
-    { email, password }
+    'http://localhost:5004/api/auth/login',
+    { email, password },
+    { withCredentials: true }
   );
   return res.data;
 };
