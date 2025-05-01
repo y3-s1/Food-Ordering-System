e1b869df765c5a68bbf189b9ae8e2c975c4fd685
SandeepKahawatta <sandeepkahawatta9@gmail.com>
2025-05-01 23:35:47 +0530

    add feature to get user location to create order

---
+++ client/src/components/order/location/MapPicker.tsx
@@ -0,0 +1,42 @@
+// src/components/MapPicker.tsx
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+interface MapPickerProps {
+  position: { lat: number; lng: number };
+  onChange: (pos: { lat: number; lng: number }) => void;
+}
+
+function ClickHandler({ onChange }: Pick<MapPickerProps, 'onChange'>) {
+  useMapEvents({
+    click(e) {
+      onChange({ lat: e.latlng.lat, lng: e.latlng.lng });
+    }
+  });
+  return null;
+}
+
+export function MapPicker({ position, onChange }: MapPickerProps) {
+  return (
+    <MapContainer
+      center={[position.lat, position.lng]}
+      zoom={13}
+      style={{ height: 300, width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker
+        position={[position.lat, position.lng]}
+        draggable
+        eventHandlers={{
+          dragend(e) {
+            const { lat, lng } = e.target.getLatLng();
+            onChange({ lat, lng });
+          }
+        }}
+      />
+      <ClickHandler onChange={onChange} />
+    </MapContainer>
+  );
+}
