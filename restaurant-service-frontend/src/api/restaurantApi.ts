62d186869c387153d95720efe8431c2dc74b2043
Thilan Randika <dgtrandika@gmail.com>
2025-05-03 14:09:48 +0530

    Change restaurant frontend base URL to API gateway

--- restaurant-service-frontend/src/api/restaurantApi.ts
+++ restaurant-service-frontend/src/api/restaurantApi.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 
 const restaurantApi = axios.create({
-  baseURL: 'http://localhost:5001/api/restaurants',
+  baseURL: `${API_BASE_URL}/api/restaurants/api/restaurants`,
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default restaurantApi;
